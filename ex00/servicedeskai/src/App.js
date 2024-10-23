41045769ae397a7080f8616ead506f3aac8eabde
Alejandro Cardenas Parejo <alcarden@c1r14s5.42malaga.com>
2024-10-23 13:12:37 +0200

    first_app

---
+++ ex00/servicedeskai/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/counterSlice';
+
+function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <h1>Count: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
