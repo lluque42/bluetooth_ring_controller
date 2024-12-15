45c7fbb0101d99a80ada8f146b6ecac1d0a9daec
Luis Luque <luque.luis@gmail.com>
2024-12-15 11:10:56 +0100

    ADDed integration of BT capabilities to touch
    Now the prototype sends a testing gesture sequence via Bluetooth
    when any of the capacitive touch circuits is touched.

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -12,7 +12,7 @@ $ sudo adduser lluque dialout
 $ # (and every time it's unplugged/plugged)
 $ sudo chmod a+rw /dev/ttyUSB0
 */
-
+/*
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
@@ -30,6 +30,21 @@ class MyServerCallbacks: public BLEServerCallbacks {
       deviceConnected = false;
     }
 };
+*/
+
+#include "BluetoothSerial.h"
+
+// Check if Bluetooth is available
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+// Check Serial Port Profile
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT;
 
 // set pin numbers
 const int touch_0 = 4;
@@ -45,7 +60,7 @@ const int touch_9 = 32;
 
 const int serial_speed = 115200;
 const int capacitive_touch_input_number = 8;
-\
+
 // The delay (ms) in the loop() function that determines
 // the sample frequency of the capacitive touch inputs.
 const int sampling_period = 200;
@@ -239,9 +254,8 @@ void  gestureInterpreter(int *states) {
 // The code that will be run once at power on
 void setup()
 {
-  // BT begins
-  Serial.begin(115200);
   
+  /*
   BLEDevice::init("ESP32");
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
@@ -260,8 +274,13 @@ void setup()
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->start();
   //BT end
+  */
+
   // Serial communication speed
   Serial.begin(serial_speed);
+  delay(1000);
+  // Initialize bluetooth connection using this device name
+  init_bt("Filigring");
   calc_baseline(baseline);
   Serial.println("Ring on line!!!");
 }
@@ -277,6 +296,51 @@ void loop()
   //print_array("value_", values, capacitive_touch_input_number);
   get_touch_states(values, states);
 
+  // The other end must send something after stablishing the connection with the Filigring
+  // for the SerialBT.available() function to start returning true!!!! 
+  while (!SerialBT.available())
+  {
+    Serial.println("Waiting for a Bluetooth connection... The other end must send something after succesfully pairing and connecting with the Filigring");
+    delay(3000);
+  }
+  //Serial.println("ERROR! Bluetooth unavailable");
+  /*
+  send_char_bt('t', 5000);
+  send_char_bt('v', 5000);
+  ...
+  */
+  //send_test_gestures_bt(5000);
+  i = 0;
+  int pressed = 0;
+  int pressed_count = 0;
+
+  while (i < 8)
+  {
+    if (pressed == 0 && states[i] == 1)
+    {
+      pressed = 1;
+      send_custom_gestures_bt("tvvv^^vvvccdc^^vv", 500);
+      break ;
+    }
+    i++;
+  }
+  if (pressed == 1)
+  {
+    pressed_count = 0;
+    int j = 0;
+    while (j < 8)
+    {
+      pressed_count += states[i];
+      j++;
+    }
+    if (pressed_count == 0)
+    {
+      pressed = 0;
+    }
+  }
+  
+
+/*
   // BT code begins
    if (deviceConnected) {
     // Cuando quieras enviar una señal de captura
@@ -286,7 +350,74 @@ void loop()
     delay(1000); // Ajusta según necesites
   }
   // BT code ends
+*/
  // gestureInterpreter(states);
   print_array("state_", states, capacitive_touch_input_number);
   delay(sampling_period);
 }
+
+// Initializes bluetooth connection using the argument as the device name
+void  init_bt(char *device_name)
+{
+  // Uncomment this to delete paired devices; Must be called after begin
+  SerialBT.deleteAllBondedDevices();
+  SerialBT.begin(device_name);
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name);
+}
+
+int send_test_gestures_bt(int delay_ms)
+{
+  delay(delay_ms);
+  send_char_bt('t', 500);
+  send_char_bt('v', 200);
+  send_char_bt('v', 200);
+  send_char_bt('v', 200);
+  send_char_bt('c', 200);
+  return (1);
+}
+
+int send_custom_gestures_bt(std::string gestures, int delay_ms)
+{
+  int i;
+  int length;
+  /*
+  const char *gestures_cstr;
+
+  gestures_cstr = gestures.c_str();
+  */
+  length = gestures.size();
+  i = 0;
+  while (i < length)
+  {
+    send_char_bt(gestures[i], delay_ms);
+    //Serial.printf("i = %d, char sent = '%c'\n", i, gestures[i]);
+    i++;
+  }
+  
+  return (1);
+  /*
+  {
+    send_char_bt(gestures_cstr[i], delay_ms);
+    Serial.printf("i = %d\n", i);
+    i++;
+  }
+  */
+}
+
+int send_char_bt(char c, int delay_ms)
+{
+  if (!SerialBT.available())
+  {
+    Serial.println("ERROR! Bluetooth unavailable");
+    return (0);
+  }
+  //Serial.printf("Received '%s'\n", SerialBT.read());
+  SerialBT.write(c);
+  //Serial.printf("Char '%c' sent, now waiting 20ms or %dms\n", c, delay_ms);
+  if (delay_ms < 20)
+    delay(20);
+  else
+    delay(delay_ms);
+  return (1);
+}
+
